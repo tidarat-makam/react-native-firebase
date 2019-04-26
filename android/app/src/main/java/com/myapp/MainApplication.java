b7e59e5d126fc6eb61c8fa09d08108f53fa054d1
Max <max30012540@hotmail.com>
2019-04-26 15:31:17 +0700

    Revert "push noti"
    This reverts commit a6af73882ebaa711a03f98c07e40a33fc09a0515.

--- android/app/src/main/java/com/myapp/MainApplication.java
+++ android/app/src/main/java/com/myapp/MainApplication.java
@@ -3,9 +3,6 @@ package com.myapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-
 import com.pilloxa.backgroundjob.BackgroundJobPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
@@ -29,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativePushNotificationPackage(),
             new BackgroundJobPackage(),
             new RNFirebasePackage(),
               new MapsPackage()
